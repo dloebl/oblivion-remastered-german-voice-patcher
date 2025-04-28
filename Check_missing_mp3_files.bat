8f3762e116b8197eaaa2575bdc61f8b17769632a
Leischii <yannickaaron@gmail.com>
2025-04-28 21:12:18 +0200

    add: alt race and alt version support

---
+++ Check_missing_mp3_files.bat
@@ -0,0 +1,8 @@
+:: This bat is only used to start the script with busybox
+
+
+:: Check for bnks without mp3 counterpart
+.\busybox\busybox.exe bash scripts\check-missing-mp3s.sh
+
+pause
+exit
--- scripts/change-prefix-move-mp3s.sh
+++ scripts/change-prefix-move-mp3s.sh
@@ -13,18 +13,68 @@ mv tmp/sound/voice/knights.esp/rothwardone tmp/sound/voice/knights.esp/redguard
 mv tmp/sound/voice/oblivion.esm/dunkler* tmp/sound/voice/oblivion.esm/dark_seducer
 mv tmp/sound/voice/oblivion.esm/goldener* tmp/sound/voice/oblivion.esm/golden_saint
 
-# copy all audio files to the MP3 to WEM input folder
+# Check if bnk with matching prefix exists and copy to the MP3 to WEM input folder
+check_and_copy_mp3() {
+	filename="${3##*/}"
+	filename="${filename%.mp3}"
+	counter=0
+	
+    echo "Check: ${1}_${2}_${3}..."
+	
+	# Check for normal, altvoice and beggar variant
+	for prefix in "" "_altvoice" "_beggar"
+	do
+		# Check if bnk matching the mp3s name exists to prevent unused mp3 copies
+		if [ -f "tmp/pak/OblivionRemastered/Content/WwiseAudio/Event/English(US)/Play_${1}_${2}${prefix}_${filename}.bnk" ]; then
+			echo "Found BNK: (${1}_${2}${prefix}_${3}) -> Copy MP3..."
+			cp "$4" "tmp/MP3s/${1}_${2}${prefix}_${3}" &
+		else
+			counter=$((counter + 1))
+		fi
+	done
+	
+	# Check if mp3 was used, write to log file if not
+	if [ "$counter" -eq 3 ]; then
+		echo "No matching bnk found!"
+		echo "${1}_${2}_${3}" >> notused.txt
+	fi
+}
+
+# If bnk counterpart is found copy mp3s to the folder with files to convert
+# Also add files for alt races that don't have individual VO 
 mkdir -p tmp/MP3s/
 for dlc in tmp/sound/voice/*
 do
-    for race in $dlc/*
-    do
-        for variant in $race/*
-        do
-            for file in $variant/*.mp3
-            do
-                cp $file tmp/MP3s/"${race##*/}_${variant##*/}_${file##*/}" &
-            done
-        done
-    done
+	for race in $dlc/*
+	do
+		for variant in $race/*
+		do
+			for file in $variant/*.mp3
+			do
+				# Check if mp3 has bnk counterpart
+				check_and_copy_mp3 "${race##*/}" "${variant##*/}" "${file##*/}" "$file"
+				
+				# Check if mp3 has alternative race bnk counterpart
+				case "${race##*/}" in
+                    "argonian")
+                        check_and_copy_mp3 "khajiit" "${variant##*/}" "${file##*/}" "$file"
+                        ;;
+                    "high_elf")
+                        check_and_copy_mp3 "dark_elf" "${variant##*/}" "${file##*/}" "$file"
+                        check_and_copy_mp3 "wood_elf" "${variant##*/}" "${file##*/}" "$file"
+                        ;;
+                    "imperial")
+                        check_and_copy_mp3 "breton" "${variant##*/}" "${file##*/}" "$file"
+                        ;;
+                    "nord")
+                        check_and_copy_mp3 "orc" "${variant##*/}" "${file##*/}" "$file"
+                        ;;
+                    *)
+                        ;;
+                esac
+			done
+		done
+	done
 done
+
+wait
