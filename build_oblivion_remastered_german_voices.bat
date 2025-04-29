0dc72061c9320322e8edc0b7fc3e1f5132819e8c
Leischii <yannickaaron@gmail.com>
2025-04-29 17:21:49 +0200

    fix: mod result build

--- build_oblivion_remastered_german_voices.bat
+++ build_oblivion_remastered_german_voices.bat
@@ -5,7 +5,6 @@ chcp 1252
 set DIRECTORY_ORIGINAL=F:\Steam\SteamApps\common\Oblivion\Data
 set DIRECTORY_OBRE=F:\Steam\SteamApps\common\Oblivion Remastered\OblivionRemastered\Content
 set UNREAL_BIN_DIR=F:\UE_5.5\Engine\Binaries\Win64
-set RESULT_FOLDER=german-voices-oblivion-remastered-voxmeld_v0.2.2_P
 
 set VOICES_1_BSA_ORIGINAL=%DIRECTORY_ORIGINAL%\Oblivion - Voices1.bsa
 set VOICES_2_BSA_ORIGINAL=%DIRECTORY_ORIGINAL%\Oblivion - Voices2.bsa
@@ -18,14 +17,19 @@ set SHIVERING_ISLES_BSA_OBRE=%DIRECTORY_OBRE%\Dev\ObvData\Data\DLCShiveringIsles
 set KNIGHTS_BSA_OBRE=%DIRECTORY_OBRE%\Dev\ObvData\Data\Knights.bsa
 set OBRE_PAK=%DIRECTORY_OBRE%\Paks\OblivionRemastered-Windows.pak
 
+set RESULT_FOLDER_DATA=ModFiles\Oblivion Remastered\OblivionRemastered\Content\Dev\ObvData\Data
+set RESULT_FOLDER_PAK=ModFiles\Oblivion Remastered\OblivionRemastered\Content\Paks\~mods
+
+:: Create folders for temp files and final mod files
 mkdir tmp\
-mkdir %RESULT_FOLDER%\
+mkdir %RESULT_FOLDER_DATA%\
+mkdir %RESULT_FOLDER_PAK%\
 
 :: Extract the remaster .bsa files with VO
-.\BSArch\BSArch.exe unpack "%VOICES_1_BSA_OBRE%" "%RESULT_FOLDER%\" -mt
-.\BSArch\BSArch.exe unpack "%VOICES_2_BSA_OBRE%" "%RESULT_FOLDER%\" -mt
-.\BSArch\BSArch.exe unpack "%SHIVERING_ISLES_BSA_OBRE%" "%RESULT_FOLDER%\" -mt
-.\BSArch\BSArch.exe unpack "%KNIGHTS_BSA_OBRE%" "%RESULT_FOLDER%\" -mt
+.\BSArch\BSArch.exe unpack "%VOICES_1_BSA_OBRE%" "%RESULT_FOLDER_DATA%\" -mt
+.\BSArch\BSArch.exe unpack "%VOICES_2_BSA_OBRE%" "%RESULT_FOLDER_DATA%\" -mt
+.\BSArch\BSArch.exe unpack "%SHIVERING_ISLES_BSA_OBRE%" "%RESULT_FOLDER_DATA%\" -mt
+.\BSArch\BSArch.exe unpack "%KNIGHTS_BSA_OBRE%" "%RESULT_FOLDER_DATA%\" -mt
 :: Extract the original MP3s from all original .bsa voice files
 set TMP_DIR=%CD%\tmp\
 .\BSArch\BSArch.exe unpack "%VOICES_1_BSA_ORIGINAL%" tmp\ -mt
@@ -40,18 +44,12 @@ set UNREAL_PAK_EXE=%UNREAL_BIN_DIR%\UnrealPak.exe
 :: Convert all MP3s to WEMs with Vorbis codec (this is going to take quite a while)
 set TMP_DIR=%CD%\tmp\
 cmd /c .\sound2wem\sound2wem.cmd "%TMP_DIR%\MP3s\*"
-
 mkdir sound2wem\WindowsFinal\
 :: Duplicate wem files if variant exists
 .\busybox\busybox.exe bash scripts\duplicate-wems-altraces-variants.sh
 :: Patch the BNKs, update the WEMs file names and copy everything to the output folder in one go
 .\busybox\busybox.exe bash scripts\patch-bnks-copy-out.sh
-:: Rename .bsa files of remaster so extract folder can be used
-rename "%VOICES_1_BSA_OBRE%" "Oblivion - Voices1_bak.bsa"
-rename "%VOICES_2_BSA_OBRE%" "Oblivion - Voices2_bak.bsa"
-rename "%SHIVERING_ISLES_BSA_OBRE%" "DLCShiveringIsles - Voices_bak.bsa"
-rename "%KNIGHTS_BSA_OBRE%" "Knights_bak.bsa"
 :: Final step. Build the mod PAK file
-cmd /c .\scripts\create_pak.bat "%CD%\%RESULT_FOLDER%\"
+cmd /c .\scripts\create_pak.bat "%CD%\%RESULT_FOLDER_PAK%\"
 pause
 exit
