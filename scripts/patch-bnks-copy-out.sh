22d387ff922735feb48e36e8e319e8c164cb25d2
Daniel Löbl <dloebl.2000@gmail.com>
2025-05-10 17:02:32 +0200

    remove unnecessary script

--- scripts/patch-bnks-copy-out.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# this is there the new BNKs go
-mkdir -p "german-voices-oblivion-remastered-voxmeld_v0.4.3_P/Content/WwiseAudio/Event/English(US)/"
-# this is there the new WEMs go
-mkdir -p "german-voices-oblivion-remastered-voxmeld_v0.4.3_P/Content/WwiseAudio/Media/English(US)/"
-# loop over all WEMs that we managed to convert to
-find sound2wem/Windows/ -name "*.wem" | sed "s/\.wem$//" | xargs -n 1 -P 64 -I {} ./voxmeld/voxmeld.exe {}
